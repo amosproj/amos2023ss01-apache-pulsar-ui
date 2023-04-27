2c32e820fefc56080005252fa765d5bca3cac163
JulianTS <julian.tochmanszewc@gmail.com>
2023-04-27 10:14:38 +0200

    #9: React setup with eslint and prettier
    Signed-off-by: JulianTS <julian.tochmanszewc@gmail.com>

---
+++ frontend/src/reportWebVitals.ts
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2010-2021 Dirk Riehle <dirk@riehle.org
+// SPDX-FileCopyrightText: 2019 Georg Schwarz <georg. schwarz@fau.de>
+import { ReportHandler } from 'web-vitals'
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+	if (onPerfEntry && onPerfEntry instanceof Function) {
+		import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+			getCLS(onPerfEntry)
+			getFID(onPerfEntry)
+			getFCP(onPerfEntry)
+			getLCP(onPerfEntry)
+			getTTFB(onPerfEntry)
+		})
+	}
+}
+
+export default reportWebVitals
