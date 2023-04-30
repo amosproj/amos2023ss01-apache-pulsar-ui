e1462ec12a2abf4f5802d22d64294440296b3c4a
JulianTS <julian.tochmanszewc@gmail.com>
2023-04-30 14:29:28 +0200

    #9: fix remaining linting errors
    Signed-off-by: JulianTS <julian.tochmanszewc@gmail.com>

--- frontend/components/Link.tsx
+++ frontend/components/Link.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useState } from 'react'
 
+//TODO correct type
+// prettier-ignore
+interface LinkProps {
+	page?: any
+	children: any
+}
+
 const STATUS = {
 	HOVERED: 'hovered',
 	NORMAL: 'normal',
 }
 
-export default function Link({ page, children }) {
+export default function Link({ page, children }: LinkProps) {
 	const [status, setStatus] = useState(STATUS.NORMAL)
 
 	const onMouseEnter = () => {
