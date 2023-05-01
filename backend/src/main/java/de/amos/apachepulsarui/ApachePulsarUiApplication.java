aa247413f398b11a94767b684c8497abd9ff0caa
Niklas Teschner <niklas.teschner@web.de>
2023-05-01 12:39:27 +0200

    #13 Fix directory structure.

---
+++ backend/src/main/java/de/amos/apachepulsarui/ApachePulsarUiApplication.java
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2023 Niklas Teschner <niklas.teschner@web.de>
+ * SPDX-FileCopyrightText: 2023 Jonas Arnhold <jonasarnhold@web.de>
+ */
+
+package de.amos.apachepulsarui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachePulsarUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApachePulsarUiApplication.class, args);
+	}
+
+}
