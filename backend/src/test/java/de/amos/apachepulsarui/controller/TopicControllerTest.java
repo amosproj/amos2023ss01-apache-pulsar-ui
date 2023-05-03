1a40029f1b1633535b903e034a28980192b8b7d6
ahaverkamp <anna.lucia.haverkamp@gmail.com>
2023-05-03 17:46:37 +0200

    Added TopicController Test
    Signed-off-by: Anna Haverkamp <anna.lucia.haverkamp@gmail.com>

---
+++ backend/src/test/java/de/amos/apachepulsarui/controller/TopicControllerTest.java
@@ -0,0 +1,38 @@
+package de.amos.apachepulsarui.controller;
+
+import de.amos.apachepulsarui.service.TopicService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TopicController.class)
+public class TopicControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    TopicService topicService;
+
+    @Test
+    void getAllTopics_returnsAllTopics() throws Exception {
+        var topics = List.of("Tatooine", "Naboo", "Coruscant");
+        Mockito.when(topicService.getAllTopics()).thenReturn(topics);
+
+        mockMvc.perform(get("/topic")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.topics", equalTo(topics)));
+    }
+}
