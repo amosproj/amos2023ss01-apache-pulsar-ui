1114c09a97649da4d40a48f3d11dfe22d9091dc5
Jonas Arnhold <jonasarnhold@web.de>
2023-05-04 12:51:20 +0200

    #22: Make newTopic route a PostMapping
    Signed-off-by: Jonas Arnhold <jonasarnhold@web.de>

--- backend/src/main/java/de/amos/apachepulsarui/controller/TopicController.java
+++ backend/src/main/java/de/amos/apachepulsarui/controller/TopicController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,7 +29,7 @@ public class TopicController {
         return new ResponseEntity<>(new TopicsDto(topicService.getAllTopics()), HttpStatus.OK);
     }
 
-    @GetMapping("/{topic}")
+    @PostMapping("/{topic}")
     public ResponseEntity<Void> newTopic(@PathVariable String topic) {
         if (topicService.createNewTopic(topic)) {
             return new ResponseEntity<>(HttpStatus.OK);
