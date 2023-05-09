b6a3e5ec338567d4236c9cbece0d8f0563e8e41c
Sharaz Nasir <nasirsharaz@gmail.com>
2023-05-09 15:32:13 +0200

    Added test for Form Component
    Signed-off-by: Sharaz Nasir <nasirsharaz@gmail.com>

--- frontend/src/__tests__/Form.test.tsx
+++ frontend/src/__tests__/Form.test.tsx
@@ -4,10 +4,54 @@ import Form from '../components/form/Form'
 import { Provider } from 'react-redux'
 import store from '../store'
 
-test('should add element by submitting form', async () => {
+const triggerUpdateTest = (msg: string, tpc: string) => {
+	console.log(msg, tpc)
+}
+
+const dataTest = [
+	{
+		id: '100',
+		name: 'Topic 1',
+		messages: [
+			{
+				id: '1001',
+				value: 'Test value 1',
+				topic: '100',
+			},
+			{
+				id: '1002',
+				value: 'Test value 2',
+				topic: '100',
+			},
+			{
+				id: '1003',
+				value: 'Test value 3',
+				topic: '100',
+			},
+		],
+	},
+	{
+		id: '200',
+		name: 'Topic 2',
+		messages: [
+			{
+				id: '2001',
+				value: 'Test value 1',
+				topic: '200',
+			},
+			{
+				id: '2002',
+				value: 'Test value 2',
+				topic: '200',
+			},
+		],
+	},
+]
+
+test('should add values to inputs and submit form', async () => {
 	const formComponent = render(
 		<Provider store={store}>
-			<Form />
+			<Form data={dataTest} triggerUpdate={triggerUpdateTest} />
 		</Provider>
 	)
 	const selectinput = formComponent.getByTestId('demo-simple-select')
@@ -19,6 +63,4 @@ test('should add element by submitting form', async () => {
 	expect((textinput as HTMLInputElement).value).toBe('Test with Jest')
 	const form = await formComponent.findByTestId('demo-message-form')
 	fireEvent.submit(form)
-
-	expect(formComponent).toMatchSnapshot()
 })
