91d6705c0f89de8dbf7941e846c4f615349a57ec
JulianTS <julian.tochmanszewc@gmail.com>
2023-05-10 23:31:04 +0200

    #37: implement navbar in App.tsx
    Signed-off-by: JulianTS <julian.tochmanszewc@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,37 @@
 import React, { useEffect } from 'react'
 import './App.css'
 import './assets/styles/styles.scss'
-import logo from './assets/images/team-logo-light.png'
 import Form from './components/form/Form'
 import { useAppDispatch, useAppSelector } from './store/hooks'
-import { selectData, setData, updateData } from './store/globalSlice'
+import {
+	selectData,
+	selectView,
+	setData,
+	updateData,
+} from './store/globalSlice'
+import ResponsiveNavBar from './components/responsiveNavBar'
+
+type SampleViewData = {
+	tag: string
+	id: number
+	content: string
+}
+let allData: Array<SampleViewData> = []
 
 function App() {
 	const dispatch = useAppDispatch()
 	const dummyData = useAppSelector(selectData)
-
 	const triggerUpdate = (msg: string, tpc: string) => {
 		dispatch(updateData({ message: msg, topic: tpc }))
 		console.log(dummyData)
 	}
+
+	const view = useAppSelector(selectView)
+
+	const filteredData = allData.filter(
+		//TODO add cases with item.id
+		(item) => !view || item.tag === view.selectedNav
+	)
 
 	//can later on be replaced by the fetchDataThunk
 	const getData = () => {
@@ -35,6 +53,21 @@ function App() {
 				console.log(json)
 				dispatch(setData(json))
 			})
+		//part for dummy NavBar
+		fetch('dummy/dummyViewData.json', {
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+		})
+			.then(function (response) {
+				console.log(response)
+				return response.json()
+			})
+			.then(function (json) {
+				console.log(json)
+				allData = json
+			})
 	}
 
 	useEffect(() => {
@@ -44,9 +77,12 @@ function App() {
 	return (
 		<div className="bg-blue w-full h-full">
 			<div className="w-full h-full">
-				<nav className="px-12 py-5">
-					<img className="home-logo" src={logo} alt="logo" />
-				</nav>
+				<ResponsiveNavBar></ResponsiveNavBar>
+				<ul>
+					{filteredData.map((item, index) => (
+						<li key={index}>{item.content}</li>
+					))}
+				</ul>
 				<Form data={dummyData} triggerUpdate={triggerUpdate}></Form>
 			</div>
 		</div>
