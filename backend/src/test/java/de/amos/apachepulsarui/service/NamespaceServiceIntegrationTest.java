c67073f8dd96a634c4a622febe6c41988d6a72f8
Anna Haverkamp <anna.lucia.haverkamp@gmail.com>
2023-05-12 22:03:15 +0200

    #44: Add integration test for NamespaceService
    Signed-off-by: Anna Haverkamp <anna.lucia.haverkamp@gmail.com>

---
+++ backend/src/test/java/de/amos/apachepulsarui/service/NamespaceServiceIntegrationTest.java
@@ -0,0 +1,61 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2023 Anna Haverkamp <anna.lucia.haverkamp@gmail.com>
+ */
+
+package de.amos.apachepulsarui.service;
+
+import de.amos.apachepulsarui.dto.NamespaceDto;
+import de.amos.apachepulsarui.dto.TenantDto;
+import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.apache.pulsar.client.admin.PulsarAdminException;
+import org.apache.pulsar.common.policies.data.TenantInfo;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.List;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class NamespaceServiceIntegrationTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private NamespaceService namespaceService;
+
+    @Autowired
+    private PulsarAdmin pulsarAdmin;
+
+    @BeforeAll
+    void createTenantsAndNamespaces() throws PulsarAdminException {
+        createTenant("tenant1");
+        createTenant("tenant2");
+        pulsarAdmin.namespaces().createNamespace("tenant1/namespace1");
+        pulsarAdmin.namespaces().createNamespace("tenant1/namespace2");
+        pulsarAdmin.namespaces().createNamespace("tenant2/namespace3");
+    }
+
+    @Test
+    void getAllNamespaces_returnsNamespaces() {
+        List<NamespaceDto> namespaces = namespaceService.getAll();
+        var namespaceIds = namespaces.stream().map(NamespaceDto::getId).toList();
+        Assertions.assertThat(namespaceIds).contains("tenant1/namespace1", "tenant1/namespace2", "tenant2/namespace3");
+    }
+
+    @Test
+    void getAllNamespacesOfTenant_returnsNamespacesOfTenant() {
+        List<NamespaceDto> namespaces = namespaceService.getAllOfTenant(TenantDto.fromString("tenant1"));
+        var namespaceIds = namespaces.stream().map(NamespaceDto::getId).toList();
+        Assertions.assertThat(namespaceIds).contains("tenant1/namespace1", "tenant1/namespace2");
+        Assertions.assertThat(namespaceIds).doesNotContain("tenant2/namespace3");
+    }
+
+    private void createTenant(String tenant) throws PulsarAdminException {
+        var clusters = pulsarAdmin.clusters().getClusters();
+        pulsarAdmin.tenants().createTenant(tenant, TenantInfo.builder()
+                .allowedClusters(new HashSet<>(clusters)).build());
+    }
+
+}
