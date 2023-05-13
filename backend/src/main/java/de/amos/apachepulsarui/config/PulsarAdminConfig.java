1d7f369a78a813266bca255952d4fc4e6b0da5b4
Niklas Teschner <niklas.teschner@web.de>
2023-05-13 16:39:52 +0200

    #45 Revert Changes.
    Signed-off-by: Niklas Teschner <niklas.teschner@web.de>

--- backend/src/main/java/de/amos/apachepulsarui/config/PulsarAdminConfig.java
+++ backend/src/main/java/de/amos/apachepulsarui/config/PulsarAdminConfig.java
@@ -5,8 +5,8 @@
 
 package de.amos.apachepulsarui.config;
 
+import lombok.SneakyThrows;
 import org.apache.pulsar.client.admin.PulsarAdmin;
-import org.apache.pulsar.client.api.PulsarClientException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +18,8 @@ public class PulsarAdminConfig {
     private String adminUrl;
 
     @Bean
-    public PulsarAdmin createPulsarAdmin() throws PulsarClientException {
+    @SneakyThrows
+    public PulsarAdmin createPulsarAdmin() {
         boolean tlsAllowInsecureConnection = false;
         String tlsTrustCertsFilePath = null;
         return PulsarAdmin.builder()
