c03fda2f733e83057b0ed7a544249666e92af38a
Niklas Teschner <niklas.teschner@web.de>
2023-05-18 20:15:11 +0200

    #68 Rebase Changes
    Signed-off-by: Niklas Teschner <niklas.teschner@web.de>

--- backend/src/test/java/de/amos/apachepulsarui/controller/TopicControllerTest.java
+++ backend/src/test/java/de/amos/apachepulsarui/controller/TopicControllerTest.java
@@ -50,7 +50,7 @@ public class TopicControllerTest {
                 "persistent://public/default/coruscant"
         ).map( values -> TopicDto.createTopicDto(values, topicStats, RandomString.make(1))).toList();
 
-        Mockito.when(topicService.getAllByNamespace("public/default")).thenReturn(topics);
+        when(topicService.getAllByNamespace("public/default")).thenReturn(topics);
 
         mockMvc.perform(get("/topic/public/default")
                 .contentType(MediaType.APPLICATION_JSON))
