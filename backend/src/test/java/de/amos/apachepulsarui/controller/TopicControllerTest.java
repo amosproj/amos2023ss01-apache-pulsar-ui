2c60a9dca1ed262c3eb106123c512cf873300e14
Niklas Teschner <niklas.teschner@web.de>
2023-05-24 21:36:39 +0200

    #56#57 Add Subscriber and Publisher
    Signed-off-by: Niklas Teschner <niklas.teschner@web.de>

--- backend/src/test/java/de/amos/apachepulsarui/controller/TopicControllerTest.java
+++ backend/src/test/java/de/amos/apachepulsarui/controller/TopicControllerTest.java
@@ -5,11 +5,14 @@
 
 package de.amos.apachepulsarui.controller;
 
+import de.amos.apachepulsarui.dto.ProducerDto;
+import de.amos.apachepulsarui.dto.SubscriptionDto;
 import de.amos.apachepulsarui.dto.TopicDto;
 import de.amos.apachepulsarui.service.NamespaceService;
 import de.amos.apachepulsarui.service.TenantService;
 import de.amos.apachepulsarui.service.TopicService;
 import net.bytebuddy.utility.RandomString;
+import org.apache.pulsar.common.policies.data.SubscriptionStats;
 import org.apache.pulsar.common.policies.data.TopicStats;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,19 +34,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TopicControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @MockBean
-    TopicService topicService;
+    private TopicService topicService;
+
+    @MockBean
+    private NamespaceService namespaceService;
 
     @MockBean
     TenantService tenantService;
 
     @MockBean
-    NamespaceService namespaceService;
+    private TopicStats topicStats;
 
     @MockBean
-    TopicStats topicStats;
+    private SubscriptionStats subscriptionStats;
 
     @Test
     void returnAllTopicsByNamespace() throws Exception {
@@ -52,12 +58,12 @@ public class TopicControllerTest {
                 "persistent://public/default/tatooine",
                 "non-persistent://public/default/naboo",
                 "persistent://public/default/coruscant"
-        ).map( values -> TopicDto.createTopicDto(values, topicStats, RandomString.make(1))).toList();
+        ).map(values -> TopicDto.createTopicDto(values, topicStats, RandomString.make(1))).toList();
 
         when(topicService.getAllByNamespace("public/default")).thenReturn(topics);
 
         mockMvc.perform(get("/topic/public/default")
-                .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.topics[0].name", equalTo(topics.get(0).getName())))
                 .andExpect(jsonPath("$.topics[1].name", equalTo(topics.get(1).getName())))
@@ -77,4 +83,35 @@ public class TopicControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", equalTo(fullTopic)));
     }
+
+    @Test
+    void getSubscriptionByNameAndTopic() throws Exception {
+        String subscription = "R2D2";
+        String topic = "Droide";
+
+        SubscriptionDto subscriptionDto = SubscriptionDto.createSubscriptionDto(subscriptionStats, subscription);
+
+
+        when(topicService.getSubscriptionByTopic(topic, subscription)).thenReturn(subscriptionDto);
+
+        mockMvc.perform(get("/topic/" + topic + "/subscription/" + subscription)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(subscriptionDto)));
+    }
+
+    @Test
+    void getProducerByNameAndTopic() throws Exception {
+        String producer = "C3PO";
+        String topic = "Droide";
+        ProducerDto dto = new ProducerDto(producer, null, 0);
+
+        when(topicService.getProducerByTopic(topic, producer)).thenReturn(dto);
+
+        mockMvc.perform(get("/topic/" + topic + "/producer/" + producer)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(producer)));
+    }
+
 }
