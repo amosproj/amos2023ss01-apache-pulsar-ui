1f47c83c81195d446478b33a4e6b76fab0759fd2
Jonas Arnhold <jonasarnhold@web.de>
2023-05-25 09:49:09 +0200

    #25: Enable CORS for all origins
    Signed-off-by: Jonas Arnhold <jonasarnhold@web.de>

--- backend/src/main/java/de/amos/apachepulsarui/config/WebConfig.java
+++ backend/src/main/java/de/amos/apachepulsarui/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("*");
     }
 
