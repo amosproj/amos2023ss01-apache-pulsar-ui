57ef07ba7429a7c537534da35d1a72fd0d076c28
JulianTS <julian.tochmanszewc@gmail.com>
2023-06-01 20:45:53 +0200

    #105: update view of cluster, tenant, namespace and topic
    Signed-off-by: JulianTS <julian.tochmanszewc@gmail.com>

--- frontend/src/components/views/ClusterView.tsx
+++ frontend/src/components/views/ClusterView.tsx
@@ -1,77 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
 import graph1 from '../../assets/images/demo-graph1.png'
 import graph2 from '../../assets/images/demo-graph2.png'
+import { Button, CardActions, Collapse } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 
 const ClusterView: React.FC<ClusterViewProps> = ({ data, handleClick }) => {
+	const [expanded, setExpanded] = useState(false)
+
+	const handleExpand = () => {
+		//TODO if(!data) fetch detailed data
+		setExpanded(!expanded)
+	}
 	return (
 		<div className="flex flex-col card-content">
 			<h2 className="uppercase">{data.id}</h2>
-			<div className="flex card-inner">
-				<div className="flex flex-col card-col card-col-1">
-					<div className="flex flex-col card-info">
-						<p className="text-black">
-							Brokers:{' '}
-							<span className="text-blue">
-								{data?.brokers?.length > 0 ? data.brokers.length : 0}
-							</span>
-						</p>
-						<p className="text-black">
-							Bookies:{' '}
-							<span className="text-blue">
-								{data?.bookies
-									? data.bookies.length
+			<div className="grey-line"></div>
+			<Collapse in={expanded} timeout="auto" unmountOnExit>
+				<div className="flex card-inner">
+					<div className="flex flex-col card-col card-col-1">
+						<div className="flex flex-col card-info">
+							<p className="text-black">
+								Brokers:{' '}
+								<span className="text-blue">
+									{data?.brokers?.length > 0 ? data.brokers.length : 0}
+								</span>
+							</p>
+							<p className="text-black">
+								Bookies:{' '}
+								<span className="text-blue">
+									{data?.bookies
 										? data.bookies.length
-										: 0
-									: 0}
-							</span>
-						</p>
+											? data.bookies.length
+											: 0
+										: 0}
+								</span>
+							</p>
+						</div>
+						<div className="grey-line"></div>
+						<div className="flex flex-col card-info">
+							<p className="text-black">
+								Namespaces:{' '}
+								<span className="text-blue">
+									{data?.amountOfNamespaces ? data.amountOfNamespaces : 0}
+								</span>
+							</p>
+							<p className="text-black">
+								Topics:{' '}
+								<span className="text-blue">
+									{data?.amountOfTopics ? data.amountOfTopics : 0}
+								</span>
+							</p>
+						</div>
+						<div className="grey-line"></div>
+						<div className="flex flex-col card-info">
+							<p className="text-black">
+								Service URL:{' '}
+								<span className="text-blue">
+									{data?.serviceUrl ? data.serviceUrl : 'N/A'}
+								</span>
+							</p>
+							<p className="text-black">
+								Broker Service URL:{' '}
+								<span className="text-blue">
+									{data?.brokerServiceUrl ? data.brokerServiceUrl : 'N/A'}
+								</span>
+							</p>
+						</div>
 					</div>
-					<div className="grey-line"></div>
-					<div className="flex flex-col card-info">
-						<p className="text-black">
-							Namespaces:{' '}
-							<span className="text-blue">
-								{data?.amountOfNamespaces ? data.amountOfNamespaces : 0}
-							</span>
-						</p>
-						<p className="text-black">
-							Topics:{' '}
-							<span className="text-blue">
-								{data?.amountOfTopics ? data.amountOfTopics : 0}
-							</span>
-						</p>
-					</div>
-					<div className="grey-line"></div>
-					<div className="flex flex-col card-info">
-						<p className="text-black">
-							Service URL:{' '}
-							<span className="text-blue">
-								{data?.serviceUrl ? data.serviceUrl : 'N/A'}
-							</span>
-						</p>
-						<p className="text-black">
-							Broker Service URL:{' '}
-							<span className="text-blue">
-								{data?.brokerServiceUrl ? data.brokerServiceUrl : 'N/A'}
-							</span>
-						</p>
-					</div>
-					<div>
-						<a
-							href="#"
-							className="primary-button"
-							onClick={(e) => handleClick(e, data)}
+				</div>
+			</Collapse>
+			<div className="flex justify-between">
+				{' '}
+				<CardActions disableSpacing>
+					{expanded ? (
+						<Button
+							variant={'contained'}
+							style={{ marginRight: '10px' }}
+							onClick={handleExpand}
+							endIcon={<ExpandLessIcon />}
 						>
-							More information
-						</a>
-					</div>
-				</div>
-				<div className="flex flex-col card-col card-col-2">
-					<img className="my-auto" src={graph2}></img>
-				</div>
-				<div className="flex flex-col card-col card-col-3">
-					<img className="my-auto" src={graph1}></img>
-				</div>
+							Hide
+						</Button>
+					) : (
+						<Button
+							variant={'contained'}
+							style={{ marginRight: '10px' }}
+							onClick={handleExpand}
+							endIcon={<ExpandMoreIcon />}
+						>
+							show details
+						</Button>
+					)}
+					<Button variant={'contained'} onClick={(e) => handleClick(e, data)}>
+						drill down
+					</Button>
+				</CardActions>
 			</div>
 		</div>
 	)
--- frontend/src/components/views/TenantView.tsx
+++ frontend/src/components/views/TenantView.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import graph1 from '../../assets/images/demo-graph1.png'
 import graph2 from '../../assets/images/demo-graph2.png'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
+import { Collapse, CardActions, Button } from '@mui/material'
 
 const TenantView: React.FC<TenantViewProps> = ({ data, handleClick }) => {
 	const tenantAdminRoles = data?.tenantInfo?.adminRoles
+
+	const [expanded, setExpanded] = useState(false)
+
+	const handleExpand = () => {
+		//TODO if(!data) fetch detailed data
+		setExpanded(!expanded)
+	}
 
 	return (
 		<div className="flex flex-col card-content">
@@ -18,7 +28,7 @@ const TenantView: React.FC<TenantViewProps> = ({ data, handleClick }) => {
 							</span>
 						</p>
 						<p className="text-black">
-							Admin Rolas:{' '}
+							Admin Roles:{' '}
 							<span className="text-blue">
 								{tenantAdminRoles &&
 									tenantAdminRoles.length > 0 &&
@@ -29,36 +39,55 @@ const TenantView: React.FC<TenantViewProps> = ({ data, handleClick }) => {
 						</p>
 					</div>
 					<div className="grey-line"></div>
-					<div className="flex flex-col card-info">
-						<p className="text-black">
-							Namespaces:{' '}
-							<span className="text-blue">
-								{data?.amountOfNamespaces ? data.amountOfNamespaces : 0}
-							</span>
-						</p>
-						<p className="text-black">
-							Topics:{' '}
-							<span className="text-blue">
-								{data?.amountOfTopics ? data.amountOfTopics : 0}
-							</span>
-						</p>
+				</div>
+			</div>
+			<div className="grey-line"></div>
+			<Collapse in={expanded} timeout="auto" unmountOnExit>
+				<div className="flex card-inner">
+					<div className="flex flex-col card-col card-col-1">
+						<div className="flex flex-col card-info">
+							<p className="text-black">
+								Namespaces:{' '}
+								<span className="text-blue">
+									{data?.amountOfNamespaces ? data.amountOfNamespaces : 0}
+								</span>
+							</p>
+							<p className="text-black">
+								Topics:{' '}
+								<span className="text-blue">
+									{data?.amountOfTopics ? data.amountOfTopics : 0}
+								</span>
+							</p>
+						</div>
 					</div>
-					<div>
-						<a
-							href="#"
-							className="primary-button"
-							onClick={(e) => handleClick(e, data)}
+				</div>
+			</Collapse>
+			<div className="flex justify-between">
+				{' '}
+				<CardActions disableSpacing>
+					{expanded ? (
+						<Button
+							variant={'contained'}
+							style={{ marginRight: '10px' }}
+							onClick={handleExpand}
+							endIcon={<ExpandLessIcon />}
 						>
-							More information
-						</a>
-					</div>
-				</div>
-				<div className="flex flex-col card-col card-col-2">
-					<img className="my-auto" src={graph2}></img>
-				</div>
-				<div className="flex flex-col card-col card-col-3">
-					<img className="my-auto" src={graph1}></img>
-				</div>
+							Hide
+						</Button>
+					) : (
+						<Button
+							variant={'contained'}
+							style={{ marginRight: '10px' }}
+							onClick={handleExpand}
+							endIcon={<ExpandMoreIcon />}
+						>
+							show details
+						</Button>
+					)}
+					<Button variant={'contained'} onClick={(e) => handleClick(e, data)}>
+						drill down
+					</Button>
+				</CardActions>
 			</div>
 		</div>
 	)
