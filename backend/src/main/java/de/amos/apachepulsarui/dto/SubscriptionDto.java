07110f569836af602efe8fe381c7e4a487b70448
Niklas Teschner <niklas.teschner@web.de>
2023-06-01 11:12:31 +0200

    #90 Update subscription endpoint
    Signed-off-by: Niklas Teschner <niklas.teschner@web.de>

--- backend/src/main/java/de/amos/apachepulsarui/dto/SubscriptionDto.java
+++ backend/src/main/java/de/amos/apachepulsarui/dto/SubscriptionDto.java
@@ -8,6 +8,7 @@ package de.amos.apachepulsarui.dto;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
+import org.apache.pulsar.common.policies.data.ConsumerStats;
 import org.apache.pulsar.common.policies.data.SubscriptionStats;
 
 import java.util.List;
@@ -19,9 +20,9 @@ public class SubscriptionDto {
 
     private String name;
 
-    private ConsumerDto activeConsumer;
+    private String activeConsumer;
 
-    private List<ConsumerDto> allConsumers;
+    private List<String> allConsumers;
 
     private List<MessageDto> messages;
 
@@ -31,13 +32,21 @@ public class SubscriptionDto {
 
     private long msgBacklog;
 
+    private long backlogSize;
+
     private long msgOutCounter;
 
+    private long bytesOutCounter;
+
+    private boolean replicated;
+
+    private String type;
+
     public static SubscriptionDto create(SubscriptionStats subscriptionStats, List<MessageDto> messages, String name) {
-        List<ConsumerDto> consumers = getConsumers(subscriptionStats);
-        ConsumerDto active = consumers
+        List<String> consumers = getConsumers(subscriptionStats);
+        String active = consumers
                 .stream()
-                .filter(c -> Objects.equals(c.getName(), subscriptionStats.getActiveConsumerName()))
+                .filter(c -> Objects.equals(c, subscriptionStats.getActiveConsumerName()))
                 .findFirst()
                 .orElse(null);
 
@@ -48,14 +57,18 @@ public class SubscriptionDto {
                 .allConsumers(consumers)
                 .msgAckRate(subscriptionStats.getMessageAckRate())
                 .msgBacklog(subscriptionStats.getMsgBacklog())
-                .msgOutCounter(builder().msgOutCounter)
+                .bytesOutCounter(subscriptionStats.getBytesOutCounter())
+                .replicated(subscriptionStats.isReplicated())
+                .msgOutCounter(subscriptionStats.getMsgOutCounter())
+                .type(subscriptionStats.getType())
+                .backlogSize(subscriptionStats.getBacklogSize())
                 .build();
     }
 
-    private static List<ConsumerDto> getConsumers(SubscriptionStats subscriptionStats) {
+    private static List<String> getConsumers(SubscriptionStats subscriptionStats) {
         return subscriptionStats.getConsumers()
                 .stream()
-                .map(ConsumerDto::create)
+                .map(ConsumerStats::getConsumerName)
                 .toList();
 
     }
