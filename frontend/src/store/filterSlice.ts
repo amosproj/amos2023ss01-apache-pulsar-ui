1471649bb796b393b024cf7c536a7b8daf185ac8
JulianTS <julian.tochmanszewc@gmail.com>
2023-06-12 23:39:16 +0200

    #123: restructure custom filter and remove depricated elements

--- frontend/src/store/filterSlice.ts
+++ frontend/src/store/filterSlice.ts
@@ -11,11 +11,23 @@ export type FilterState = {
 	namespace: string[]
 	topic: string[]
 	message: string[]
+	displayedOptions: {
+		allClusters: string[]
+		allTenants: string[]
+		allNamespaces: string[]
+		allTopics: string[]
+		allMessages: string[]
+	}
 }
 
 type UpdateSingleFilter = {
 	filterName: 'cluster' | 'tenant' | 'namespace' | 'topic' | 'message'
 	id: string
+}
+
+type UpdateDisplayedOptions = {
+	topologyLevel: 'cluster' | 'tenant' | 'namespace' | 'topic' | 'message'
+	options: string[]
 }
 
 const initialState: FilterState = {
@@ -24,6 +36,13 @@ const initialState: FilterState = {
 	namespace: [],
 	topic: [],
 	message: [],
+	displayedOptions: {
+		allClusters: [],
+		allTenants: [],
+		allNamespaces: [],
+		allTopics: [],
+		allMessages: [],
+	},
 }
 
 const filterSlice = createSlice({
@@ -59,6 +78,35 @@ const filterSlice = createSlice({
 			state,
 			action: PayloadAction<UpdateSingleFilter>
 		) => {
+			switch (action.payload.filterName) {
+				case 'cluster':
+					state.cluster = initialState.cluster
+					state.cluster.push(action.payload.id)
+					break
+				case 'tenant':
+					state.tenant = initialState.tenant
+					state.tenant.push(action.payload.id)
+					break
+				case 'namespace':
+					state.namespace = initialState.namespace
+					state.namespace.push(action.payload.id)
+					break
+				case 'topic':
+					state.topic = initialState.topic
+					state.topic.push(action.payload.id)
+					break
+				case 'message':
+					state.message = initialState.message
+					state.message.push(action.payload.id)
+					break
+				default:
+					console.log(
+						'wrong type for updateDisplayedOptions with' +
+							action.payload.filterName +
+							' ' +
+							action.payload.id
+					)
+			}
 			const filterName = action.payload.filterName
 			const query = action.payload.id
 			state[filterName] = state[filterName].filter((element) => {
@@ -66,7 +114,41 @@ const filterSlice = createSlice({
 			})
 		},
 		resetAllFilters: (state) => {
-			state = initialState
+			//to not accidently delete the displayed options:
+			state.cluster = initialState.cluster
+			state.tenant = initialState.tenant
+			state.namespace = initialState.namespace
+			state.topic = initialState.topic
+			state.message = initialState.message
+		},
+		updateDisplayedOptions: (
+			state,
+			action: PayloadAction<UpdateDisplayedOptions>
+		) => {
+			switch (action.payload.topologyLevel) {
+				case 'cluster':
+					state.displayedOptions.allClusters = action.payload.options
+					break
+				case 'tenant':
+					state.displayedOptions.allTenants = action.payload.options
+					break
+				case 'namespace':
+					state.displayedOptions.allNamespaces = action.payload.options
+					break
+				case 'topic':
+					state.displayedOptions.allTopics = action.payload.options
+					break
+				case 'message':
+					state.displayedOptions.allMessages = action.payload.options
+					break
+				default:
+					console.log(
+						'wrong type for updateDisplayedOptions with' +
+							action.payload.topologyLevel +
+							' ' +
+							action.payload.options
+					)
+			}
 		},
 	},
 })
@@ -84,7 +166,45 @@ const selectNamespace = (state: RootState): string[] => {
 const selectTopic = (state: RootState): string[] => {
 	return state.filterControl.topic
 }
-export { selectCluster, selectNamespace, selectTenant, selectTopic }
+
+const selectOptions = (
+	state: RootState
+): {
+	allClusters: string[]
+	allTenants: string[]
+	allNamespaces: string[]
+	allTopics: string[]
+	allMessages: string[]
+} => {
+	return state.filterControl.displayedOptions
+}
+
+const selectAllFilters = (
+	state: RootState
+): {
+	cluster: string[]
+	tenant: string[]
+	namespace: string[]
+	topic: string[]
+	message: string[]
+} => {
+	return {
+		cluster: state.filterControl.cluster,
+		tenant: state.filterControl.tenant,
+		namespace: state.filterControl.namespace,
+		topic: state.filterControl.topic,
+		message: state.filterControl.message,
+	}
+}
+
+export {
+	selectCluster,
+	selectNamespace,
+	selectTenant,
+	selectTopic,
+	selectOptions,
+	selectAllFilters,
+}
 
 export const {
 	setCluster,
@@ -95,6 +215,7 @@ export const {
 	deleteFilter,
 	addFilterByDrillDown,
 	resetAllFilters,
+	updateDisplayedOptions,
 } = filterSlice.actions
 
 export default filterSlice.reducer
