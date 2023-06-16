45685faa505521d3e41a0fe180e8433046be0061
Jonas Arnhold <jonasarnhold@web.de>
2023-06-16 13:17:52 +0200

    #114: Fix sorting of immutable list by stream-sorting
    Signed-off-by: Jonas Arnhold <jonasarnhold@web.de>

--- backend/src/main/java/de/amos/apachepulsarui/dto/TopicDetailDto.java
+++ backend/src/main/java/de/amos/apachepulsarui/dto/TopicDetailDto.java
@@ -22,45 +22,50 @@ public class TopicDetailDto {
 
     private String name;
 
-	private String localName;
+    private String localName;
 
-	private String namespace;
+    private String namespace;
 
-	private String tenant;
+    private String tenant;
 
-	private boolean isPersistent;
+    private boolean isPersistent;
 
-	private String ownerBroker;
+    private String ownerBroker;
 
-	private TopicStatsDto topicStatsDto;
+    private TopicStatsDto topicStatsDto;
 
-	private List<SchemaInfoDto> schemaInfos = new ArrayList<>();
+    private List<SchemaInfoDto> schemaInfos = new ArrayList<>();
 
-	public static TopicDetailDto create(
+    public static TopicDetailDto create(
             String completeTopicName,
             TopicStats topicStats,
             String ownerBroker,
             List<SchemaInfoDto> schemaInfos
-	) {
-		TopicName topicName = TopicName.get(completeTopicName);
-		TopicDetailDto topicDetailDto = new TopicDetailDto();
+    ) {
+        TopicName topicName = TopicName.get(completeTopicName);
+        TopicDetailDto topicDetailDto = new TopicDetailDto();
 
-		topicDetailDto.name = topicName.toString();
-		topicDetailDto.localName = topicName.getLocalName();
-		topicDetailDto.namespace = topicName.getNamespacePortion();
-		topicDetailDto.tenant = topicName.getTenant();
-		topicDetailDto.isPersistent = topicName.isPersistent();
-		topicDetailDto.ownerBroker = ownerBroker;
+        topicDetailDto.name = topicName.toString();
+        topicDetailDto.localName = topicName.getLocalName();
+        topicDetailDto.namespace = topicName.getNamespacePortion();
+        topicDetailDto.tenant = topicName.getTenant();
+        topicDetailDto.isPersistent = topicName.isPersistent();
+        topicDetailDto.ownerBroker = ownerBroker;
 
-		topicDetailDto.setTopicStatsDto(TopicStatsDto.createTopicStatsDto(topicStats));
+        topicDetailDto.setTopicStatsDto(TopicStatsDto.createTopicStatsDto(topicStats));
 
-		// we want to put the latest schema version on top of the list
-		if (!schemaInfos.isEmpty()) {
-			schemaInfos.sort(Comparator.comparing(SchemaInfoDto::getVersion, Collections.reverseOrder()));
-		}
-		topicDetailDto.setSchemaInfos(schemaInfos);
+        // we want to put the latest schema version on top of the list
+        if (!schemaInfos.isEmpty()) {
+            List<SchemaInfoDto> schemaInfosSorted = schemaInfos.stream()
+                    .sorted(Comparator.comparing(SchemaInfoDto::getVersion, Collections.reverseOrder()))
+                    .toList();
+            topicDetailDto.setSchemaInfos(schemaInfosSorted);
+        } else {
+			topicDetailDto.setSchemaInfos(schemaInfos);
+        }
 
-		return topicDetailDto;
-	}
+
+        return topicDetailDto;
+    }
 
 }
