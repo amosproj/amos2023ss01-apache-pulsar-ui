75c586d26d1ac059b63c915fc4321fa631936766
Ubuntu <ubuntu@ip-172-31-0-228.eu-north-1.compute.internal>
2023-06-22 21:06:26 +0000

    #142: pass env as build arg to frontend and make it accessible for React

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-const backendUrl = process.env.BACKEND_URL || 'http://localhost:8081'
+const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8081'
 
 export default {
 	backendUrl,
