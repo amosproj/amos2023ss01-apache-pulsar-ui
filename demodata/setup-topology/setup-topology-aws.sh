2d4d98077fd07496e6c9a261165e82c4822e5229
Jonas Arnhold <jonasarnhold@web.de>
2023-06-23 11:19:06 +0200

    #142: Adjust shell script to create 5k topics
    Signed-off-by: Jonas Arnhold <jonasarnhold@web.de>

--- demodata/setup-topology/setup-topology-aws.sh
+++ demodata/setup-topology/setup-topology-aws.sh
@@ -1,30 +1,30 @@
-bin/pulsar-admin --admin-url $PULSAR_ADMIN_URL topics list tenant-1/namespace-1
-if [ $? -eq 0 ]; then    
-    echo "Topics found. Skipping topology setup."
+bin/pulsar-admin --admin-url "$PULSAR_ADMIN_URL" topics list tenant-1/namespace-1
+if [ $? -eq 0 ]; then
+  echo "Topics found. Skipping topology setup."
 else
-    echo "Setting up topology."
 
-    for i in {1..20}
-    do 
+  echo "Setting up topology."
+  x=0
 
-        echo "Creating tenant-$i"
-        bin/pulsar-admin --admin-url $PULSAR_ADMIN_URL tenants create tenant-$i
-        
-        for j in {1..10}
-        do
-            x=$(((i-1)*10+j))
-            echo "Creating tenant-$i/namespace-$x"
-            bin/pulsar-admin --admin-url $PULSAR_ADMIN_URL namespaces create tenant-$i/namespace-$x
+  for i in {1..10}; do
 
-            for k in {1..25}
-            do 
+    echo "Creating tenant-$i"
+    bin/pulsar-admin --admin-url "$PULSAR_ADMIN_URL" tenants create "tenant-$i"
 
-                y=$(((x-1)*25+k))
-                echo "Creating tenant-$i/namespace-$x/topic-$y"
-                bin/pulsar-admin --admin-url $PULSAR_ADMIN_URL topics create tenant-$i/namespace-$x/topic-$y
-            done
+    for j in {1..20}; do
 
-        done
-        
+      echo "Creating tenant-$i/namespace-$j"
+      bin/pulsar-admin --admin-url "$PULSAR_ADMIN_URL" namespaces create "tenant-$i/namespace-$j"
+
+      for k in {1..25}; do
+
+        x=$((x + 1))
+        echo "Creating tenant-$i/namespace-$j/topic-$x"
+        bin/pulsar-admin --admin-url "$PULSAR_ADMIN_URL" topics create "tenant-$i/namespace-$j/topic-$x"
+
+      done
+
     done
+
+  done
 fi
