002944c851361ec58a6e8feac9846e6e1b4977cb
JulianTS <julian.tochmanszewc@gmail.com>
2023-07-08 11:46:29 +0200

    refactor: remove deprecated code
    Signed-off-by: JulianTS <julian.tochmanszewc@gmail.com>

--- frontend/src/components/InfoModal.tsx
+++ frontend/src/components/InfoModal.tsx
@@ -22,17 +22,6 @@ export function InfoModal() {
 	const handleClose = () => {
 		setOpen(false)
 	}
-	const style = {
-		position: 'absolute' as const,
-		top: '50%',
-		left: '50%',
-		transform: 'translate(-50%, -50%)',
-		width: 400,
-		bgcolor: 'background.paper',
-		border: '2px solid #000',
-		boxShadow: 24,
-		p: 4,
-	}
 
 	return (
 		<div className="info-modal-wrapper">
