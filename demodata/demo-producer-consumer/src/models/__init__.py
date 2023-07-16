89a7549b11131499b3e68020262e9d0d23643b57
Anna Haverkamp <anna.lucia.haverkamp@gmail.com>
2023-07-16 13:49:16 +0200

    Adjust demo data and producer/consumer script to story, parse message payload

--- demodata/demo-producer-consumer/src/models/__init__.py
+++ demodata/demo-producer-consumer/src/models/__init__.py
@@ -1,5 +1,4 @@
 
-from .createCustomer import CreateCustomer, CreateCustomerSerDe
-from .customerEvent import CustomerEvent, CustomerEventSerDe
+from .TransactionEvent import TransactionEvent, TransactionEventSerDe
 from .messageProperties import MessageProperties
 from .pulsarClientProperties import PulsarClientProperties
